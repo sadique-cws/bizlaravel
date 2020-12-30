1357f48c53dae2766c238a26fa36b58bebd66576
sadique-cws <sadique.hussain96@gmail.com>
2020-12-30 08:54:26 +0530

    model creation

---
+++ app/Http/Controllers/BizRecordController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BizRecord;
+use Illuminate\Http\Request;
+
+class BizRecordController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\BizRecord  $bizRecord
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BizRecord $bizRecord)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\BizRecord  $bizRecord
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BizRecord $bizRecord)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\BizRecord  $bizRecord
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BizRecord $bizRecord)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\BizRecord  $bizRecord
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BizRecord $bizRecord)
+    {
+        //
+    }
+}
